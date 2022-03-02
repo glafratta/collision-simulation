6d7292a12da3e5f84b8b0c96094f85180e5187cb
glafratta <g.lafratta@outlook.com>
2022-03-02 10:34:28 +0000

    debugging

--- lidar.h
+++ lidar.h
@@ -8,6 +8,7 @@
 #include <iomanip>
 #include <sstream> //for writing string into file, for checking, std::ostringstream
 //#define title(name, number,) name##number
+#include <iostream>
 
 class DataInterface : public A1Lidar::DataInterface{
 public: 
@@ -20,30 +21,31 @@ public:
     }
 
 
-	void newScanAvail(float, A1LidarData (&data)[A1Lidar::nDistance]){
+	void newScanAvail(float, A1LidarData (&data)[A1Lidar::nDistance]){ //should print map name and coordinates to the console
+        numberOfScans++;
         box2d->createMap();
         Map * map = box2d->maps->at(box2d->maps->size()-1);
+        std::cout<<"map in box2d->maps\n";
         std::stringstream tmp;
         tmp << "map" << std::setw(4) << std::setfill('0') << numberOfScans << ".dat";
-        //std::string filename = tmp.str();
         const char * filename = tmp.str().c_str();
-        FILE * file =fopen(filename, "w+");
-        numberOfScans++;
+        std::cout<<filename<<std::endl;
+//        FILE * file =fopen(filename, "w+"); //uncomment
 		for (A1LidarData &data:data){
 			if (data.valid){
                 std::ostringstream stream;
-                //std::ostringstream yStream;                
                 Obstacle* obstacle = new Obstacle(*(box2d->world), approximate(data.x), approximate(data.y)); 
                 obstacle->body->SetAwake(true);
 			    map->obstacles->push_back(obstacle);
-                stream <<obstacle->body->GetPosition().x<<"\t"<<obstacle->body->GetPosition().y<<"\n";
-                //std::string line = stream.str();
-                const char * line = stream.str().c_str();
-                fputs(line, file);
-                fclose(file);
+                //stream <<obstacle->body->GetPosition().x<<"\t"<<obstacle->body->GetPosition().y<<"\n";
+                std::cout<<obstacle->body->GetPosition().x<<"\t"<<obstacle->body->GetPosition().y<<"\n";
+//                const char * line = stream.str().c_str();
+                // fputs(line, file);
+                // fclose(file);
+                // printf("hi");
 			}
 		}
-        box2d->simulate(); //simulates collision for 5 seconds
+        //box2d->simulate(); //simulates collision for 5 seconds
 
 	}
 
