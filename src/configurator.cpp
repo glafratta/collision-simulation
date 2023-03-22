5513166a231472409e421d2ab978f43ed68be00d
glafratta <g.lafratta@outlook.com~>
2023-03-22 15:47:38 +0000

    moved findBestBranch outside if(currentDMP.change) loop for debugging

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -141,11 +141,11 @@ void Configurator::NewScan(std::vector <Point> & data){
 			break;
 		case 1:
 			currentDMP.change = build_tree(v0, g, currentDMP, world, leaves); //for now should produce the same behaviour because the tree is not being pruned. original build_tree returned bool, now currentDMP.change is changed directly
+			edgeDescriptor e = findBestBranch(g, leaves);
+			Primitive::Direction dir = g[e].direction;
 			if (currentDMP.change){
 				//see search algorithms for bidirectional graphs (is this like incorrect bonkerballs are mathematicians going to roast me)
 				//FIND BEST OPTION FOR CHANGING
-				edgeDescriptor e = findBestBranch(g, leaves);
-				Primitive::Direction dir = g[e].direction;
 				currentDMP = Primitive(g[v0].obstacle, dir); //new currentDMP has the obstacle of the previous and the direction of the edge remaining 
 			}
 			break;
