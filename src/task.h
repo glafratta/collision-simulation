4ddc034bd957a0cee93cc78d21c8f923406ec6f0
glafratta <g.lafratta@outlook.com~>
2023-06-12 15:02:14 +0100

    general header in task.h

--- src/task.h
+++ src/task.h
@@ -7,6 +7,7 @@
 #include "measurement.h"
 #include <stdexcept>
 #include "settings.h"
+#include "general.h" //general functions + point class + typedefs + Primitive.h + boost includes
 #define BOX2DRANGE 0.5
 #define LIDAR_RANGE 1.0
 #define REACTION_TIME 2.3
