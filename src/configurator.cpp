5506b72ddcd128bbd2e83fd940103dbfe064118e
glafratta <g.lafratta@outlook.com~>
2023-06-13 16:59:38 +0100

    range bsed iteration to transfer points from current to previous

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -454,7 +454,6 @@ void Configurator::run(Configurator * c){
 			printf("null pointer to configurator\n");
 		}
 		if (!(c->ci->data2fp.empty())){
-
 			if (c->ci->data != c->current){
 				printf("c->ci->data size = %i\n", c->ci->data.size());
 				c->Spawner(c->ci->data, c->ci->data2fp);
