b24db938d07e5aa9039f087a7db734962939415f
glafratta <g.lafratta@outlook.com~>
2023-06-13 14:36:24 +0100

    range bsed iteration to transfer points from current to previous

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -7,8 +7,8 @@ void Configurator::Spawner(CoordinateContainer & data, CoordinateContainer & dat
 	//PREPARE VECTORS TO RECEIVE DATA
 	iteration++; //iteration set in getVelocity
 	CoordinateContainer previous;
-	for (auto d =current.begin(); d!=current.end(); ++d){
-		previous.insert(*d);
+	for (Point d:current){
+		previous.insert(d);
 	}
 	//previous = current;
 	current.clear();
