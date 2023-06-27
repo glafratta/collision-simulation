2d2c2ff82198d4612446445858fa0282d06c6b5b
glafratta <g.lafratta@outlook.com~>
2023-06-27 16:23:13 +0100

    reaction time = 1 secon

--- src/task.h
+++ src/task.h
@@ -9,7 +9,7 @@
 #include "settings.h"
 #define BOX2DRANGE 1.0
 #define LIDAR_RANGE 1.0
-#define REACTION_TIME 2.3
+#define REACTION_TIME 1.0
 #define HZ 50.0
 const float SIM_DURATION = int(BOX2DRANGE*2 /MAX_SPEED);
 #define BACK_DISTANCE 0.1 //DEFAULT backtracking distance
