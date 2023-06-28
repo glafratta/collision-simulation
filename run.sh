976eb613c622700da17e79da0ac06bd1d0cb57b5
glafratta <g.lafratta@outlook.com>
2023-06-28 15:19:54 +0100

    commit

--- run.sh
+++ run.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 temp=$(head -n 1 /sys/class/thermal/thermal_zone0/temp)
-if [ $temp -lt 70000 ]
 echo "temperature = $temp"
+if [ $temp -lt 60000 ]
 then
 	echo "ok to run, running"
 	echo "planning on? answer 0 or 1"
 	read planning
 	echo "debug on? answer 0 or 1"
 	read debug
-	if [debug==0]
-	then
-		sudo ./navigate $debug $planning
-	else
-		echo "what file to write outputs to?"
-		read filename
-		sudo ./navigate $debug $planning $filename 
+	sudo ./navigate $debug $planning
 else
-	echo "too hot!"
+	echo "too hot! wait"
 fi
