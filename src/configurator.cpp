0c4c631991472bf0aa52ec2c8f4c83c61d10bec4
glafratta <g.lafratta@outlook.com~>
2023-08-23 18:26:16 +0100

    printing direction

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -77,6 +77,7 @@ void Configurator::Spawner(CoordinateContainer data, CoordinateContainer data2fp
 	//IF WE  ALREADY ARE IN AN OBSTACLE-AVOIDING STATE, ROUGHLY ESTIMATE WHERE THE OBSTACLE IS NOW
 	//currentTask.trackDisturbance(currentTask.disturbance, timeElapsed, deltaPose); //robot default position is 0,0
 	controlGoal.trackDisturbance(controlGoal.disturbance,timeElapsed, deltaPose);
+	printf("currentTask direction =%i\n", int(currentTask.direction));
 	bool isObstacleStillThere=constructWorldRepresentation(world, currentTask.direction, b2Transform(b2Vec2(0.0, 0.0), b2Rot(0)), &currentTask); 
 	printf("bodies = %i\n", bodies);
 	//printf("obstill there! = %i\n", isObstacleStillThere);
