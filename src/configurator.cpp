6b5ecea5b47188cb887db3ab3f36abb55d427b6d
glafratta <g.lafratta@outlook.com~>
2023-08-23 15:21:45 +0100

    printing status

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -163,7 +163,7 @@ void Configurator::Spawner(CoordinateContainer data, CoordinateContainer data2fp
 
 	// }
 	currentTask.change = collisionGraph[v0].outcome==simResult::crashed;
-	printf("change task cause it fails = %i\n", currentTask.change);
+	printf("outcome code = %i, change task cause it fails = %i\n", int(collisionGraph[v0].outcome), currentTask.change);
 	//changeTask(currentTask.change, plan, collisionGraph[v0]);
 	printf("tree size = %i, bodies = %i, plan size = %i\n", collisionGraph.m_vertices.size(), bodies, plan.size());
 	float duration=0;
