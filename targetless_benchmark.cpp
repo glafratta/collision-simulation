8a12bedb0aed2ec5036a05214d8a275a9a9f8860
glafratta <g.lafratta@outlook.com~>
2023-08-23 13:43:45 +0100

    updated stepcallback, tracking in motors thread

--- targetless_benchmark.cpp
+++ targetless_benchmark.cpp
@@ -96,10 +96,10 @@ void step( AlphaBot &motors){
 	if (c->getIteration() <=0){
 		return;
 	}
-	L= c->getTask()->getAction().getLWheelSpeed();
-	R = c->getTask()->getAction().getRWheelSpeed();	
-    motors.setRightWheelSpeed(R); //temporary fix because motors on despacito are the wrong way around
-    motors.setLeftWheelSpeed(L);
+	c->trackTaskExecution(c->getTask());	
+	c->changeTask(c->getTask().change, c->plan, c->collisionGraph[0]);
+    motors.setRightWheelSpeed(c->getTask()->getAction().getRWheelSpeed()); //temporary fix because motors on despacito are the wrong way around
+    motors.setLeftWheelSpeed(c->getTask()->getAction().getLWheelSpeed());
 	printf("step: R=%f\tL=%f, conf iteration = %i, tree size = %i\n", R, L, c->getIteration(), c->treeSize);
     //iteration++;
 }
