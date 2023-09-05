052aa02dc17e14986fe6e41887c525c6e354e615
glafratta <g.lafratta@outlook.com~>
2023-09-05 15:53:58 +0100

    e

--- src/task.cpp
+++ src/task.cpp
@@ -294,6 +294,7 @@ EndedResult Task::checkEnded(b2Transform robotTransform){ //self-ended
 		}
 	}
 	else if (fabs(fabs(robotTransform.q.GetAngle())-fabs(start.q.GetAngle()))>=M_PI_2 & getAffIndex()==int(InnateAffordances::NONE)){
+	//else if (round(tan(robotTransform.q.GetAngle())*1000)==-round(tan(start.q.GetAngle())*1000) & getAffIndex()==int(InnateAffordances::NONE)){
 		r.ended =true;
 	}
 	//if (round(robotTransform.p.Length()*100)/100>=BOX2DRANGE || fabs(fabs(robotTransform.q.GetAngle())-fabs(start.q.GetAngle()))>=M_PI_2){ //if length reached or turn
