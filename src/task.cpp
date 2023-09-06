2a0e79d4822190e196aac97df5154c6690c82c7c
glafratta <g.lafratta@outlook.com~>
2023-09-06 17:01:54 +0100

    git

--- src/task.cpp
+++ src/task.cpp
@@ -81,12 +81,13 @@ void Task::trackDisturbance(Disturbance & d, float timeElapsed, b2Transform robV
 
 void Task::trackDisturbance(Disturbance & d, Action a, int s){
 	//switch(dir){
-	d.pose.q.Set(d.pose.q.GetAngle()-(s*MOTOR_CALLBACK*a.getOmega()));
+	float angleTurned =s*MOTOR_CALLBACK*a.getOmega()
+	d.pose.q.Set(d.pose.q.GetAngle()-angleTurned);
 	//float deltaLength = d.pose.p.Length()-s*MOTOR_CALLBACK*a.getLinearSpeed();
 	printf("omega = %f, linear speed %f, pose = %f", a.getOmega(), a.getLinearSpeed(), d.pose.q.GetAngle());
 	//float angle = atan2(d.pose.p.y, d.pose.p.x);
-	d.pose.p.x-= cos(d.pose.q.GetAngle())* s*MOTOR_CALLBACK*a.getLinearSpeed();
-	d.pose.p.y -= sin(d.pose.q.GetAngle())* s*MOTOR_CALLBACK*a.getLinearSpeed();
+	d.pose.p.x-= -sin(angleTurned)* s*MOTOR_CALLBACK*a.getLinearSpeed();
+	d.pose.p.y -= cos(angleTurned)* s*MOTOR_CALLBACK*a.getLinearSpeed();
 	//}
 }
 
