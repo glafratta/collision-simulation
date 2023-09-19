53f4dd1e3cfeb4f4543689fc97deb4395ecdd1e2
glafratta <g.lafratta@outlook.com~>
2023-09-19 18:41:14 +0100

    idk

--- reactive.cpp
+++ reactive.cpp
@@ -103,7 +103,7 @@ void step( AlphaBot &motors){
 	}
 	c->trackTaskExecution(*(c->getTask()));	
 	c->changeTask(c->getTask()->change, c->plan, c->collisionGraph[0], ogStep);
-    motors.setRightWheelSpeed(c->getTask()->getAction().getRWheelSpeed()); //temporary fix because motors on despacito are the wrong way around
+	motors.setRightWheelSpeed(c->getTask()->getAction().getRWheelSpeed()); //temporary fix because motors on despacito are the wrong way around
     motors.setLeftWheelSpeed(c->getTask()->getAction().getLWheelSpeed());
 	printf("step: R=%f\tL=%f, conf iteration = %i\n", c->getTask()->getAction().getRWheelSpeed(), c->getTask()->getAction().getLWheelSpeed(), c->getIteration());
     //iteration++;
