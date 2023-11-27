b82cabac0c82065f6bc4db03c84d6aa8930342e2
glafratta <g.lafratta@outlook.com~>
2023-11-27 15:34:21 +0000

    not changing running

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -154,7 +154,7 @@ bool Configurator::Spawner(CoordinateContainer data, CoordinateContainer data2fp
 	//	printf("open stat\n");
 		fprintf(f,"%i\t%i\t%f\n", worldBuilder.getBodies(), collisionGraph.m_vertices.size(), duration);
 		fclose(f);
-		return 0; //stops when finished and doesn't execute
+		//return 0; //stops when finished and doesn't execute
 
 	}
 	bodies =0;
