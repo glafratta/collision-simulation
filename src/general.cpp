cb55dec909cc2f466fa8667f40195cd2dfe86e32
glafratta <g.lafratta@outlook.com~>
2024-02-08 11:01:34 +0000

    std

--- src/general.cpp
+++ src/general.cpp
@@ -8,7 +8,7 @@ void Node::fill(simResult result){
 	endPose = result.endPose;
 	//distanceSoFar = g[srcVertex].distanceSoFar + (round(result.distanceCovered*100))/100; //rounding to 2 decimals to eliminate floating point errors
 	outcome = result.resultCode;
-	step = roundf(float(result.step)*FRICTION)/(HZ*MOTOR_CALLBACK);
+	step = std::roundf(float(result.step)*FRICTION)/(HZ*MOTOR_CALLBACK);
 	filled=true;
 }
 
