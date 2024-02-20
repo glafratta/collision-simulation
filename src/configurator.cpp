a5fa73afa5629194a3dc051457db0c2e7061e86e
glafratta <g.lafratta@outlook.com~>
2024-02-20 16:29:40 +0000

    controller on

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -163,7 +163,7 @@ bool Configurator::Spawner(CoordinateContainer data, CoordinateContainer data2fp
 
 	//IF THE TASK DIDN'T CHANGE, CORRECT PATH
 	if (isSameTask){
-	//	currentTask.controller(timeElapsed);
+		currentTask.controller(timeElapsed);
 	}
 
 	//graph should be saved and can check, if plan actually executed successfully, the probability to transition to that state increases. Read on belief update
