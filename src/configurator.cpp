ddccfe0afaafa684c9b5728daba463df15aba5c2
glafratta <g.lafratta@outlook.com~>
2024-02-28 12:53:19 +0000

    edge reset before task

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -791,10 +791,10 @@ std::vector <vertexDescriptor> Configurator::checkPlan(b2World& world, std::vect
 			break;
 		}
 		stepDistance = simulationStep;
+		e=boost::in_edges(p[it], g).first.dereference();
 		t= Task(g[e.m_source].disturbance, g[e].direction, start, true);
 		t.check=1;
 		it++;
-		e=boost::in_edges(p[it], g).first.dereference();
 	}while (it<p.size());
 	return graphError;
 }
