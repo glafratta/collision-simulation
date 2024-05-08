b9124a100285c2aa55dc3c5f3c6fb104d0c68bb5
glafratta <g.lafratta@outlook.com~>
2024-05-08 12:53:20 +0100

    not vec pointer#

--- src/sensor.cpp
+++ src/sensor.cpp
@@ -168,14 +168,15 @@ std::pair <bool, float> PointCloudProc::findOrientation(std::vector<Pointf> vec)
 }
 
 std::vector<Pointf> PointCloudProc::setDisturbanceOrientation(Disturbance& d, CoordinateContainer data){
-	std::vector <Pointf> vec;
+	std::vector <Pointf> v;
 	if (!data.empty()){
-		vec=set2vec(data);
+		v=set2vec(data);
 	}
 	else{
-		vec=previous;
+		//v=previous;
+		v=std::vector<Pointf>(previous);
 	}
-	std::vector <Pointf> nb=neighbours(d.getPosition(), NEIGHBOURHOOD,vec);
+	std::vector <Pointf> nb= std::vector <Pointf>(neighbours(d.getPosition(), NEIGHBOURHOOD,v));
 	//cv::Rect2f rect =worldBuilder.getRect(nb);
 	std::pair<bool, float> orientation =findOrientation(nb);
 	if (orientation.first){
