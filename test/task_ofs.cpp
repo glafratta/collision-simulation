2b4213ac0dc416612c4b6c7751c715c108b6ae4d
glafratta <g.lafratta@outlook.com~>
2024-05-13 17:32:58 +0100

    updated taskofs

--- test/task_ofs.cpp
+++ test/task_ofs.cpp
@@ -4,23 +4,23 @@
 
 class Callback :public AlphaBot::StepCallback { //every 100ms the callback updates the plan
     unsigned int step=0;
+    char a='0';
 public:
 Task t=Task(STOP);
-bool print =0;
-char a='0';
 
 Callback(){}
+
 void step( AlphaBot &motors){
     step--;
-    if (step==0 &t.direction!=STOP){
-        print=1;
+    if (step==0){
+        a='0';
         t=Task(STOP);
     }
     motors.setRightWheelSpeed(c->getTask()->getAction().getRWheelSpeed()); //temporary fix because motors on despacito are the wrong way around
     motors.setLeftWheelSpeed(c->getTask()->getAction().getLWheelSpeed());
 }
 
-void setID(char _a){
+void setA(char _a){
     a=_a;
     if (a== 'l'){
         t=Task(LEFT);
@@ -34,22 +34,28 @@ void setID(char _a){
         t=Task(DEFAULT);
         step=22;
     }
+    else{
+        t=Task(STOP);
+    }
 }
 };
 
 struct CameraCallback: Libcam2OpenCV::Callback {
+    int l_count=0;
+    int r_count=0;
+    int s_count=0;
     CameraCallback(Callback * _cb):cb(_cb){}
 
 	virtual void hasFrame(const cv::Mat &frame, const libcamera::ControlList &) {
 		b2Vec2 optic_flow=imgProc.opticFlow(frame, imgProc.corners(), imgProc.previous());
 		cb->t.correct.update(optic_flow.x); //for now just going straight
-        if (cb->t.direction!=STOP){
+        //if (cb->t.direction!=STOP){
             char dumpname[50];
             sprintf(dump, "%s.txt", cb->a);
             FILE * dump=fopen(dumpname, "a+");
             fprintf(dump, "%f\t%f\n", optic_flow.x, optic_flow.y);
             fclose(dump);
-        }
+        //}
     }
 private:
 ImgProc imgProc;
@@ -65,7 +71,11 @@ int main(int argc, char** argv) {
 	motors.registerStepCallback(&cb);
 	motors.start();
 	do {
-	} while (!getchar());
+        if (getchar()){
+            char a=getchar();
+            cb.setA(a);
+        } 
+	} while(true);
 	motors.stop();
 
 }
