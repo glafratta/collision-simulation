5225754510dcd25566032607ddf74178ab6b7ba6
glafratta <g.lafratta@outlook.com~>
2024-05-30 18:12:44 +0100

    printing deltapose

--- custom.h
+++ custom.h
@@ -106,7 +106,7 @@ void step( AlphaBot &motors){
     }
 	EndedResult er = c->controlGoal.checkEnded();
 	printf("control goal start: %f, %f, %f\n");
-	printf("start vertex v=%i, position= %f, %f\n", start, transitionSystem[start].endPose.p.x,  transitionSystem[start].endPose.p.y);
+	printf("start vertex v=%i, position= %f, %f\n", start, c->transitionSystem[start].endPose.p.x,  c->transitionSystem[start].endPose.p.y);
 	if (er.ended){
 		Disturbance new_goal(PURSUE, c->transitionSystem[start].endPose.p);
 		printf("new goal v=%i, position= %f, %f\n", start, new_goal.pose().p.x, new_goal.pose().p.y);
