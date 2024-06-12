54453caee63c4eb346eddb5e82cc32b8f2a3f387
glafratta <g.lafratta@outlook.com~>
2024-06-12 16:03:55 +0100

    man why do I struggle so much with mental rotation im just a girl

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -1465,12 +1465,14 @@ void Configurator::updateGraph(TransitionSystem&g, ExecutionError error, b2Trans
 	// 				angularDisplacement); //og rot
 	// //}
 	auto vPair =boost::vertices(g);
+	float angularDisplacement =getTask()->getAction().getOmega()*MOTOR_CALLBACK +error.theta();
 	for (auto vIt= vPair.first; vIt!=vPair.second; ++vIt){ //each node is adjusted in explorer, so now we update
 		if (*vIt!=movingVertex){
-		//	g[*vIt].endPose.p.x=deltaPose.q.c*g[*vIt].endPose.p.Length()-deltaPose.p.x;
-		//	g[*vIt].endPose.p.y=deltaPose.q.s*g[*vIt].endPose.p.Length()-deltaPose.p.y;
-		//	g[*vIt].endPose.q.Set(g[*vIt].endPose.q.GetAngle()-deltaPose.q.GetAngle());
-			g[*vIt].endPose-=deltaPose;
+//			g[*vIt].endPose-=deltaPose;
+			float task_distance=g[*vIt].endPose.p.Length();
+			g[*vIt].endPose.q.Set(g[*vIt].endPose.q.GetAngle()-angularDisplacement);
+			g[*vIt].endPose.p.x=task_distance*cos(g[*vIt].endPose.q.GetAngle());
+			g[*vIt].endPose.p.y=task_distance*sin(g[*vIt].endPose.q.GetAngle());
 			if (g[*vIt].disturbance.isValid()){
 				g[*vIt].disturbance.subtractPose(deltaPose);
 			}
