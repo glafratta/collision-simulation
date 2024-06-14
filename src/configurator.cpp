853bac47482a5a30d90a701daf6a3b8709ba4a9a
glafratta <g.lafratta@outlook.com~>
2024-06-14 17:18:41 +0100

    inshallah allah deliver me

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -762,7 +762,7 @@ bool Configurator::checkPlan(b2World& world, const std::vector <vertexDescriptor
 		gt::adjustProbability(g, ep.first);
 		printf("adjust prov, it %i, p size =%i, result=%i\n", it, p.size(), result);
 		// t= Task(g[ep.first.m_source].disturbance, g[ep.first.m_target].direction, start, true);
-	}while (it<p.size()-1 & result==true);
+	}while (ep.first.m_target!=TransitionSystem::null_vertex() & result==true);
 	printf("checked\n");
 	return result;
 }
