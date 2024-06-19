abe5b943d9a0d681f601ee153c8bdbc03aa88e20
glafratta <g.lafratta@outlook.com~>
2024-06-19 19:57:26 +0100

    big print

--- test/callbacks.h
+++ test/callbacks.h
@@ -137,11 +137,11 @@ public:
         ExecutionError ee =c->trackTaskExecution(*c->getTask());
       //  Task::Action action= c->getTask()->getAction();
        // c->getTask()->correct(action, c->getTask()->motorStep);
-        EndedResult er = c->controlGoal.checkEnded(b2Transform(), UNDEFINED, true);
+        EndedResult er = c->controlGoal.checkEnded(b2Transform(), UNDEFINED, false);//true
 	    if (c->controlGoal.disturbance.isValid()){
             printf("distance from goal=%f\n", c->controlGoal.disturbance.getPosition().Length());
         }
-        if (er.ended & c->getTask()->motorStep<1){
+        if (er.ended){ //& c->getTask()->motorStep<1
             Disturbance new_goal(PURSUE, c->controlGoal.start.p, c->controlGoal.start.q.GetAngle());
 		    c->controlGoal = Task(new_goal, UNDEFINED);
             printf("goal reached\n");
