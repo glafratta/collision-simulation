3b6169f5ed0581f74283434b365839ed3b9c179f
glafratta <g.lafratta@outlook.com~>
2024-06-21 17:02:19 +0100

    i am at my wits end

--- custom.h
+++ custom.h
@@ -105,7 +105,7 @@ void step( AlphaBot &motors){
     //     printf("distance from goal=%f\n", c->controlGoal.disturbance.getPosition().Length());	
 	// }
 	EndedResult er2 = c->controlGoal.checkEnded(b2Transform(b2Vec2(0,0), b2Rot(0)), UNDEFINED, true);
-	if (er.ended || (er2.ended & c->getTask()->motorStep<1 & c->planVertices.empty())){
+	if (er.ended != (er2.ended & c->getTask()->motorStep<1 & c->planVertices.empty())){
 		printf("goal reached\n");
 		Disturbance new_goal=Disturbance(PURSUE, c->controlGoal.start.p, c->controlGoal.start.q.GetAngle());
 	//	printf("new goal position= %f, %f, %f, valid =%i\n", new_goal.pose().p.x, new_goal.pose().p.y, new_goal.pose().q.GetAngle(), new_goal.isValid());
