0df7258d879102a1bb66a16dfc02eafd5be39703
glafratta <g.lafratta@outlook.com~>
2024-06-24 16:28:50 +0100

    adjusting for angles with different sign

--- src/graphTools.cpp
+++ src/graphTools.cpp
@@ -175,10 +175,14 @@ DistanceVector StateMatcher::getDistance(State s1, State s2){
 	result[2]= s1.disturbance.getAffIndex()-s2.disturbance.getAffIndex(); //disturbance type
 	result[3]= s1.endPose.p.x-s2.endPose.p.x; //endpose x
 	result[4]=s1.endPose.p.y-s2.endPose.p.y; //endpose y
-	float diff_angle=s1.endPose.q.GetAngle()-s2.endPose.q.GetAngle(); //endpose angle
-	float diff_cos=cos(diff_angle);
-	float diff_sin=sin(diff_angle);
-	result[5]= atan2(diff_sin,diff_cos );
+	float candidate_angle=s2.endPose.q.GetAngle();
+	if (s1.endPose.q.GetAngle()<0 & candidate_angle>0){
+		candidate_angle+=2*M_PI;
+	}
+	else if(candidate_angle<0 & s1.endPose.q.GetAngle()>0){
+		candidate_angle-=2*M_PI;
+	}
+	result[5]=s1.endPose.q.GetAngle()-candidate_angle;
 	return result;
 }
 
