9a59c7f336a18a41d7b12d6af3ef9d1219e95a37
glafratta <g.lafratta@outlook.com~>
2024-06-26 17:13:40 +0100

    resetting step to tgt even if not returning

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -1336,10 +1336,10 @@ std::pair <edgeDescriptor, bool> Configurator::maxProbability(std::vector<edgeDe
 
 void Configurator::adjustStepDistance(vertexDescriptor v, TransitionSystem &g, Task * t, float& step, std::pair<bool,vertexDescriptor> tgt){
 	std::pair<edgeDescriptor, bool> ep= boost::edge(v, currentVertex, g);
-	if (tgt.first){
+	if (tgt.first & !g[v].disturbance.isValid()){
 		step = (g[v].endPose.p- g[tgt.second].endPose.p).Length();
 	}
-	if(!ep.second){ //no tgt
+	if(!ep.second){ //no tgt	
 		return; //check until needs to be checked
 	}
 	auto eb=boost::edge(currentEdge.m_source,currentEdge.m_target, transitionSystem);
