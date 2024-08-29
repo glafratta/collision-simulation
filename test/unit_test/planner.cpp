50aa3bf1b50fddfbb921d355759c85ae829945cf
glafratta <g.lafratta@outlook.com~>
2024-08-29 14:24:01 +0100

    added tests

---
+++ test/unit_test/planner.cpp
@@ -0,0 +1,58 @@
+#include "../callbacks.h"
+
+std::vector <Direction> getPlan(const TransitionSystem & g, const std::vector <vertexDescriptor>& plan, vertexDescriptor pre){
+    	std::vector <Direction>result;
+        for (vertexDescriptor v: plan){
+		std::pair <edgeDescriptor, bool> edge=boost::edge(pre, v, g);
+		if (edge.second){
+			// auto a=dirmap.find(g[edge.first].direction);
+			// printf("%i, %s, ", edge.first.m_target, (*a).second);
+            result.push_back(g[edge.first].direction);
+		}
+		pre=edge.first.m_target;
+		}
+        return result;
+
+}
+
+
+int main(int argc, char** argv){
+    
+    //we imagine that we have executed a plan and then the robot is instructed to go back on its steps
+    bool debug=0;
+    Disturbance target1;
+    std::vector <Direction> solution;
+    if (argc <2){
+        return 1;
+    }
+    if (argc>2){
+        if (atoi(argv[2])==1){
+            target1= Disturbance(PURSUE, b2Vec2(1.0,0), 0);  
+            solution={DEFAULT, LEFT, DEFAULT, RIGHT, DEFAULT, RIGHT, DEFAULT, LEFT, DEFAULT};  
+        }
+        else{
+            solution={LEFT, DEFAULT};
+        }
+    }
+    Task goal(target1,DEFAULT);
+    Configurator conf(goal);
+    conf.simulationStep=0.5;
+    ConfiguratorInterface ci;
+    conf.registerInterface(&ci);
+    DataInterface di(&ci);
+    if (argc>1){
+        di.folder=argv[1];
+        di.newScanAvail();          
+    }
+    conf.data2fp = ci.data2fp;
+    conf.addIteration();
+    b2World world(b2Vec2(0,0));
+    boost::clear_vertex(conf.movingVertex, conf.transitionSystem);
+    conf.dummy_vertex(conf.currentVertex);
+    conf.explorer(conf.currentVertex, conf.transitionSystem, *conf.getTask(), world);
+    std::vector <vertexDescriptor> plan=conf.planner(conf.transitionSystem, conf.movingVertex);
+    if (getPlan(conf.transitionSystem, plan, conf.currentVertex)!=solution){
+        return 2;
+    }
+    return 0;
+}
