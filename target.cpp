5ff658af62e003a4d6f2467dee46eb00b0e197bd
glafratta <g.lafratta@outlook.com~>
2024-09-12 18:40:12 +0100

    added done that

--- target.cpp
+++ target.cpp
@@ -2,9 +2,40 @@
 
 void forget(Configurator *c){}
 
-void Configurator::done_that(vertexDescriptor& src, bool& plan_works, b2World& world, std::vector<vertexDescriptor> &plan_provisional){
+void Configurator::done_that(vertexDescriptor& src, bool & plan_works, b2World & world, std::vector<vertexDescriptor>& plan_provisional){
+		//std::pair<bool, vertexDescriptor> been(false, TransitionSystem::null_vertex());
+		//was ve instead of src
+		std::vector <vertexDescriptor> options_src;
+		if (bool fin=controlGoal.checkEnded(transitionSystem[src], UNDEFINED, true).ended; fin && currentVertex!=movingVertex){
+		//printf("is target=%i, task ended = %i\n", target.getAffIndex()==PURSUE, fin);
+			std::vector <BodyFeatures> b_features=worldBuilder.getFeatures(data2fp, b2Transform(b2Vec2(0,0), b2Rot(0)), currentTask.direction, BOX2DRANGE);
+			//Disturbance where=controlGoal.disturbance;
+			if (!b_features.empty()){
+				State s_temp;
+				s_temp.disturbance= Disturbance(b_features[0]); //assumes 1 item length
+				bool closest_match=1;
+				findMatch(s_temp,transitionSystem, transitionSystem[movingEdge.m_source].ID, UNDEFINED, StateMatcher::DISTURBANCE, &options_src);
+				//FIND STATE WHICH matches the relationship with the disturbance
+			}
+			//been= been_there(transitionSystem, where); 
+		}
+		//printf("checked been = %i\n", been.first);
+		std::vector <std::pair <vertexDescriptor, vertexDescriptor>> toRemove;
+
+		//std::vector <vertexDescriptor> plan_provisional=planVertices;
+	//	if (been.first){
+		//	printf("provisional plan\n");
+		for (auto o:options_src){
+			plan_provisional=planner(transitionSystem, o); //been.second, been.first
+			vertexDescriptor end =*(plan_provisional.rbegin().base()-1);
+			if (controlGoal.checkEnded(transitionSystem[end]).ended && checkPlan(world, plan_provisional, transitionSystem)){
+				plan_works=true;
+				break;
+			}
+		}
 
 }
+
 
 Disturbance set_target(int& run, b2Transform start){
 
