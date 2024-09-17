55c42aa6c8fbcfd40b80f82c056e272281022702
glafratta <g.lafratta@outlook.com~>
2024-09-17 11:43:21 +0100

    tweak

--- custom.h
+++ custom.h
@@ -129,12 +129,12 @@ void step( AlphaBot &motors){
 	R= c->getTask()->getAction().getRWheelSpeed()*1.02;
 	L=c->getTask()->getAction().getLWheelSpeed()*1.05*1.02;
 	if (c->getTask()->direction==LEFT){
-		R*=1.25;
-		L*=1.25;
+		R*=1.24;
+		L*=1.24;
 	}
 	else if (c->getTask()->direction==RIGHT){
-		R*=1.125;
-		L*=1.125;
+		R*=1.15;
+		L*=1.15;
 	}
     motors.setRightWheelSpeed(R); //temporary fix because motors on despacito are the wrong way around
     motors.setLeftWheelSpeed(L);
