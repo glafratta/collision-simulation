82c7b4cd991b6de0e65271ad86e531402aa8cab6
glafratta <g.lafratta@outlook.com~>
2024-10-18 12:59:28 +0100

    changed foldername + typo

--- targetless.cpp
+++ targetless.cpp
@@ -29,7 +29,7 @@ void Configurator::done_that(vertexDescriptor& src, bool & plan_works, b2World &
 				WorldBuilder::CompareCluster compareCluster;
 				BodyFeatures closest_D= *(std::min_element(b_features.begin(), b_features.end(), compareCluster));
 				s_temp.disturbance= Disturbance(closest_D); //assumes 1 item length
-				debug::print_pose(s_temp.disturbance.pose());
+				debug::print_pose(s_temp.Dn.pose());
 				bool closest_match=true;
 				findMatch(s_temp,transitionSystem, NULL, UNDEFINED, StateMatcher::DISTURBANCE, &options_src, closest_match);
 				printf("looked for matches, potential: %i, closest =%i\n", options_src.size(), closest_match);
@@ -79,7 +79,7 @@ if (argc>2){
 		configurator.planning= atoi(argv[2]);
 	}
 	if (configurator.planning){
-		configurator.setBenchmarking(1, "targetless");
+		configurator.setBenchmarking(1, "targetless_cross");
 	}
 	if (argc>1){
 		configurator.debugOn= atoi(argv[1]);
