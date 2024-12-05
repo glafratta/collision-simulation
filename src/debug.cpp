4cd4e1e2ffe8e8960467393cc59b8b5cc8b4c31d
glafratta <g.lafratta@outlook.com~>
2024-12-05 12:47:05 +0000

    test works, not updating D

--- src/debug.cpp
+++ src/debug.cpp
@@ -61,7 +61,7 @@ void debug::print_matrix(cv::Mat m){
 	std::cout << "M = " << std::endl << " "  << m << std::endl << std::endl;
 }
 
-std::vector<b2Vec2> debug::GetBodies( b2World& w){
+std::vector<b2Vec2> debug::GetBodies(b2World& w){
 	std::vector<b2Vec2> result;
 	for (b2Body * b=w.GetBodyList(); b; b=b->GetNext()){
 		result.push_back(b->GetPosition());
