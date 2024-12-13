a5f2ef851c56eaf51ec1c6c83fbf58f139062ae5
glafratta <g.lafratta@outlook.com~>
2024-12-13 16:27:44 +0000

    desired match is disturbance if g[v] out edges have not been explored

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -460,7 +460,7 @@ std::vector <std::pair<vertexDescriptor, vertexDescriptor>>Configurator::explore
 				}
 				bool closest_match=false;
 				StateMatcher::MATCH_TYPE desired_match=StateMatcher::MATCH_TYPE::_TRUE;
-				match_setup(closest_match, desired_match, v0, plan_prov, t.direction);
+				match_setup(closest_match, desired_match, v0, plan_prov, t.direction, g);
 				std::pair<StateMatcher::MATCH_TYPE, vertexDescriptor> match=findMatch(sk.first, g, source, t.direction, desired_match, NULL, closest_match);		//, closest_match	
 				// std::pair<StateMatcher::MATCH_TYPE, vertexDescriptor> match=findMatch(sk.first, g, NULL, t.direction, desired_match, NULL, closest_match);		//, closest_match	
 				std::pair <edgeDescriptor, bool> edge(edgeDescriptor(), false);
