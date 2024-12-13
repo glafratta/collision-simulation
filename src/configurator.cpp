73c944d2435eff5e531fb933e74917b0046fea76
glafratta <g.lafratta@outlook.com~>
2024-12-13 16:41:02 +0000

    if vector capacity<out degree or if v is in the paln

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -1642,7 +1642,8 @@ void Configurator::match_setup(bool& closest_match, StateMatcher::MATCH_TYPE& de
 	}
 	auto v_it=check_vector_for(plan_prov, v);
 	if ((v==movingVertex || v==currentVertex) || v_it!=plan_prov.end() ){ //|| !plan_prov.empty()
-		if (g[v].options.capacity() <= boost::out_degree(v, g)){
+		int out_deg = boost::out_degree(v, g);
+		if (g[v].options.capacity() < out_deg || v_it!=plan_prov.end()){
 			desired_match=StateMatcher::MATCH_TYPE::DISTURBANCE;
 		}
 		if (v==currentVertex && dir==currentTask.direction ){ //!plan_prov.empty() || dir==currentTask.direction
