2118ed97dda4b800994b8f2ce2a05fcdec97d4da
glafratta <g.lafratta@outlook.com~>
2025-01-25 14:21:56 +0000

    folder

--- targetless.cpp
+++ targetless.cpp
@@ -79,7 +79,7 @@ if (argc>2){
 		configurator.planning= atoi(argv[2]);
 	}
 	if (configurator.planning){
-		configurator.setBenchmarking(1, "targetless");
+		configurator.setBenchmarking(1, "s2-cds");
 	}
 	if (argc>1){
 		configurator.debugOn= atoi(argv[1]);
