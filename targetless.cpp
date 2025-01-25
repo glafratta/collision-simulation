d9eb76fe7edfa8a3e386966c1699107fad3d72ba
glafratta <g.lafratta@outlook.com~>
2025-01-25 15:14:46 +0000

    folde

--- targetless.cpp
+++ targetless.cpp
@@ -79,7 +79,7 @@ if (argc>2){
 		configurator.planning= atoi(argv[2]);
 	}
 	if (configurator.planning){
-		configurator.setBenchmarking(1, "targetless_cross_forward");
+		configurator.setBenchmarking(1, "s4-cds);
 	}
 	if (argc>1){
 		configurator.debugOn= atoi(argv[1]);
